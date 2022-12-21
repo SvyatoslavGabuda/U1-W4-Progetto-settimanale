e1674df5da01f8532c92ad2a58baee30e5af324f
shaxa321 <amonovshahruz@gmail.com>
2022-12-21 17:45:21 +0100

    piccole migliorie

--- resultPage.js
+++ resultPage.js
@@ -3,3 +3,28 @@ let giuste = params.get("numDomCorrette");
 let sbagliate = params.get("numDomSbagliate");
 let totali = params.get("numeroTotaleDomande");
 console.log(giuste, sbagliate, totali);
+
+const addValuestoHtml=function(){
+	const textWrongPercentage=document.querySelector("#wrongPercentage");
+	const textWrongAnswers=document.querySelector("#wrongAnswers")
+	const textCorrectAnswers=document.querySelector("#correctAnswers");
+	const textCorrectPercentage=document.querySelector("#correctPercentage")
+
+	const wrongpercentage=(sbagliate*100)/totali;
+	const correctpercentage=(giuste*100)/totali;
+	textWrongPercentage.innerText=`${wrongpercentage}%`
+	textCorrectPercentage.innerText=`${correctpercentage}%`
+	textCorrectAnswers.innerText=`${giuste} / ${totali} answers`
+	textWrongAnswers.innerText=`${sbagliate} / ${totali} answers`
+	
+}
+
+
+const pulsante = document.getElementById("btn")
+	  pulsante.onclick = function(){
+		console.log("ok")
+		location.href="./Feedback Page.html";
+	  }
+
+
+addValuestoHtml()
